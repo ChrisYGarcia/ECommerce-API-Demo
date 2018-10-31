5f07c46eb82c9f044446c383598784c62aa8a160
Chris Garcia <chrisgarcia769@gmail.com>
2018-10-31 01:07:35 -0500

    Implemented the product routes with a get, post, and delete request

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,54 +1,49 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
+  res.status(200).json({
+    message: "Handling GET requests to /products"
+  });
+});
+
+router.post("/", (req, res, next) => {
+  const product = {
+    name: req.body.name,
+    price: req.body.price
+  };
+
+  res.status(201).json({
+    message: "Handling GET requests to /products",
+    createdProduct: product
+  });
+});
+
+router.get("/:productId", (req, res, next) => {
+  const id = req.params.productId;
+  if (id === "special") {
     res.status(200).json({
-        message: 'Handling GET requests to /products'
-
+      message: "you discoverd the special ID",
+      id: id
     });
-});
-
-router.post('/', (req, res, next) => {
-    const product = {
-        name: req.body.name,
-        price: req.body.price
-    };
-
-    res.status(201).json({
-        message: 'Handling GET requests to /products',
-        createdProduct: product
-    });
-});
-
-router.get('/:productId', (req, res, next) => {
-    const id = req.params.productId;
-    if (id === 'special') {
-        res.status(200).json({
-            message: 'you discoverd the special ID',
-            id: id
-        });
-    }
-    else {
-        res.status(200).json({
-            message: 'You passed an ID'
-        });
-    }
-});
-
-router.patch('/:productId', (req, res, next) => {
-   res.status(200).json({
-    message: 'Updated Product!'
-
-   });
-});
-
-router.delete('/:productId', (req, res, next) => {
+  } else {
     res.status(200).json({
-     message: 'Deleted Product!'
- 
+      message: "You passed an ID"
     });
- });
+  }
+});
+
+router.patch("/:productId", (req, res, next) => {
+  res.status(200).json({
+    message: "Updated Product!"
+  });
+});
+
+router.delete("/:productId", (req, res, next) => {
+  res.status(200).json({
+    message: "Deleted Product!"
+  });
+});
 
 module.exports = router;
-
